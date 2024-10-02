163a45bf6573caeb0ebe73dbf7c0e831d753cfa1
Martin <mgarcas320@g.educaand.es>
2024-10-02 12:49:53 +0200

    virtualhost&apache

--- archivotxt/cookie.php
+++ archivotxt/cookie.php
@@ -4,14 +4,14 @@ $fecha = date("d/m/Y | H:i:s");
 
 if (isset($_COOKIE['contador'])) {
     
-    $contador   = $_COOKIE['contador'] + 1;
+    $contador = $_COOKIE['contador'] + 1;
 
 } else {
 
     
 }
 
-echo "Visita número: $contador\n";
+echo "Visita número: $contador<br>";
 echo "Su última visita fué el " . $_COOKIE['fecha'];
 
 setcookie("contador", $contador, time()+3600*24*31); //3600*24*31
@@ -19,3 +19,11 @@ setcookie("fecha", $fecha, time()+31536000);
 // unset($_COOKIE["contador"]);
 //var_dump($_COOKIE);
 //sacar cookie que diga cuando me conecté por última vez, sacar fecha y cuanas veces te has conectado.
+
+/*
+xampp
+htdocs
+ejercicio--->includes
+https://es.stackoverflow.com/questions/91530/como-manejar-rutas-de-archivos-php
+https://es.stackoverflow.com/questions/595974/problema-con-rutas-relativas
+ */
